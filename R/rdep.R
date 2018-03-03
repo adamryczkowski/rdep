36d2382b5f80daf52e5d63e893ec78ca08d8e8a5
adam <aryczkowski@imgw.pl>
2018-03-03 12:18:47 +0100

    Code remove

--- R/rdep.R
+++ R/rdep.R
@@ -1,9 +1,9 @@
-pkgs<-c('Rcpp', 'stringr', 'readxl', 'Hmisc', 'R.utils', 'xlsx', 'readODS', 'boot', 'pander',
-        'gridExtra', 'RColorBrewer', 'zipfR', 'ggrepel', 'lsr', 'tidyr', 'car', 'broom', 'ggfortify',
-        'doMC', 'ROCR', 'glmnet', 'lubridate', 'mice', 'rstan', 'HDInterval',
-        'curl', 'entropy', 'data.table', 'dplyr', 'dtplyr', 'ggplot2', 'purrr', 'purrrlyr')
-repourl<-c(CRAN="http://cran.microsoft.com")
-
-pkgList <- miniCRAN::pkgDep(pkgs, repos = repourl, type = "source", suggests = FALSE)
-
-makeRepo(pkgList, path = '/mnt/ext4/Lib/Other/r-mirror', repos = repourl, type = c("source"))
+# pkgs<-c('Rcpp', 'stringr', 'readxl', 'Hmisc', 'R.utils', 'xlsx', 'readODS', 'boot', 'pander',
+#         'gridExtra', 'RColorBrewer', 'zipfR', 'ggrepel', 'lsr', 'tidyr', 'car', 'broom', 'ggfortify',
+#         'doMC', 'ROCR', 'glmnet', 'lubridate', 'mice', 'rstan', 'HDInterval',
+#         'curl', 'entropy', 'data.table', 'dplyr', 'dtplyr', 'ggplot2', 'purrr', 'purrrlyr')
+# repourl<-c(CRAN="http://cran.microsoft.com")
+#
+# pkgList <- miniCRAN::pkgDep(pkgs, repos = repourl, type = "source", suggests = FALSE)
+#
+# makeRepo(pkgList, path = '/mnt/ext4/Lib/Other/r-mirror', repos = repourl, type = c("source"))
